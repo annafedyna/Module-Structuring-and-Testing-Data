0b28b106aa9738b8e4de3f27480ba80130741e42
Anna Fedyna <annafedyna03@gmail.com>
2024-10-30 18:49:06 +0000

    debug sp2

--- Sprint-2/debug/2.js
+++ Sprint-2/debug/2.js
@@ -2,7 +2,7 @@
 
 const num = 103;
 
-function getLastDigit() {
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
@@ -12,3 +12,7 @@ console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+
+
+/* The getLastDigit function as defined will always return the last digit of the num variable, which is 103, because it doesn't take any arguments. 
+When you call getLastDigit(42), getLastDigit(105), or getLastDigit(806), the argument is ignored, and the function uses the hardcoded value of num. */
